001c9832d42ae090608318f537b0eef1ab1cbe5c
ZharMeny <135447733+ZharMeny@users.noreply.github.com>
2024-06-30 13:16:41 +0000

    (emacs/init.el) clarify editorconfig comment

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -233,11 +233,11 @@
   :bind ("C-c n c" . denote)
   :ensure (:ref "b8fd5afe8fec763438b32bd470d6c5d690c1f43a"))
 
-;; TEMP: until Emacs 30
 (use-package editorconfig
   :custom
   (editorconfig-mode 1)
   (editorconfig-mode-lighter "")
+  ;; TEMP: until Emacs 30
   :ensure (:ref "1651294334f3987d749eeb287f11d256cf2ba5b1"))
 
 (use-package elfeed
